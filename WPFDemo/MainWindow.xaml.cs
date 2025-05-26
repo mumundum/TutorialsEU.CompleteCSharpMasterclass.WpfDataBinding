4cc1a3f48bae39ecae8573fc12237a7abeb25ad0
mumundum <196062898+mumundum@users.noreply.github.com>
2025-05-26 16:12:58 +0200

    Exercise '266. Setting up the data to bind'

--- WPFDemo/MainWindow.xaml.cs
+++ WPFDemo/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WPFDemo.Data;
 
 namespace WPFDemo
 {
@@ -20,9 +21,17 @@ namespace WPFDemo
     /// </summary>
     public partial class MainWindow : Window
     {
+        Person person = new Person
+        {
+            Name = "John Doe",
+            Age = 30
+        };
+
         public MainWindow()
         {
             InitializeComponent();
+
+            this.DataContext = person;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
